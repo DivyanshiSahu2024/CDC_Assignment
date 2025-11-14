db6961f77420004a9d37f96e14fd07cf42824121
Divyanshi Sahu <sahudiv0309@gmail.com>
2025-11-14 16:02:48 +0530

    Update and rename 724. Find Pivot Index to 724. Find Pivot Index.java

---
+++ Arrays II/724. Find Pivot Index.java
@@ -0,0 +1,18 @@
+// Problem Link:https://leetcode.com/problems/find-pivot-index/description/
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total =0;
+        for(int i=0;i<nums.length;i++){
+            total+=nums[i];
+        }
+        int leftsum=0;
+        
+        for(int i=0;i<nums.length;i++){
+            if(leftsum==total-leftsum-nums[i]){
+                return i;
+            }
+            leftsum+=nums[i];
+         }
+         return -1;
+    }
+}
