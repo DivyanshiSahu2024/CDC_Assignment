1e34b27433856e94266e7806e3e2e0db6810c537
Divyanshi Sahu <sahudiv0309@gmail.com>
2025-11-17 19:32:06 +0530

    Update 101. Symmetric Tree.java

--- Binary Tree/101. Symmetric Tree.java
+++ Binary Tree/101. Symmetric Tree.java
@@ -31,3 +31,44 @@ class Solution {
         return node1.val==node2.val && isMirror(node1.left,node2.right) && isMirror(node1.right,node2.left);
     }
 }
+
+
+
+
+
+//     [ SECOND APPROACH ]
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        invertTree(root.right);
+        return isSameTree(root.left,root.right);
+
+    }
+    private static TreeNode invertTree(TreeNode node){
+        if(node==null){
+            return null ;
+        }
+        TreeNode temp=node.left;
+        node.left=node.right;
+        node.right=temp;
+
+        node.left=invertTree(node.left);
+        node.right=invertTree(node.right);
+
+        return node;
+    }
+
+    private static boolean isSameTree(TreeNode n1, TreeNode n2 ){
+        if(n1==null && n2==null){
+            return true;
+        }
+        if(n1== null || n2==null){
+            return false;
+        }
+
+        boolean cond1=(n1.val==n2.val);
+        boolean cond2=isSameTree(n1.left,n2.left);
+        boolean cond3=isSameTree(n1.right,n2.right);
+ 
+         return cond1 && cond2 && cond3;
+    }
+}
