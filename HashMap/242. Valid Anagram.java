13689b14040c48a790874cda6c300c8fac2f3af2
Divyanshi Sahu <sahudiv0309@gmail.com>
2025-11-18 23:42:44 +0530

    Create 242. Valid Anagram.java

---
+++ HashMap/242. Valid Anagram.java
@@ -0,0 +1,23 @@
+// Problem Link: https://leetcode.com/problems/valid-anagram/
+
+import java.util.*;
+import java.io.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        
+      HashMap<Character, Integer> freq=new HashMap<>();
+      for(char c:s.toCharArray()){
+        freq.put(c,freq.getOrDefault(c,0)+1);
+      }
+
+      for(char c:t.toCharArray()){
+        if(!freq.containsKey(c)){return false;}
+        freq.put(c,freq.get(c)-1);
+        if(freq.get(c)<0){
+            return false;
+        }
+      }
+      return true;
+    }
+}
