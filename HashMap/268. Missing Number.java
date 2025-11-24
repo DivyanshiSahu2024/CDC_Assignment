8baaf64edbe279e9ca7d00b106b0fc25e0914090
Divyanshi Sahu <sahudiv0309@gmail.com>
2025-11-24 16:16:50 +0530

    Update 268. Missing Number.java

--- HashMap/268. Missing Number.java
+++ HashMap/268. Missing Number.java
@@ -1,5 +1,21 @@
 //Problem Link: https://leetcode.com/problems/missing-number/
+// simple approach----runtime 5ms
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashMap<Integer,Boolean> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+           map.put(nums[i],true);
+        }
+        for(int i=0;i<=nums.length;i++){
+            if(!map.containsKey(i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
 
+//optimized approach-----runtime 0ms
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
