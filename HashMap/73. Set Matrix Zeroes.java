e0c3f24691ade2b4e8a7bd27defda51ce2875a00
Divyanshi Sahu <sahudiv0309@gmail.com>
2025-11-24 17:10:25 +0530

    Update 73. Set Matrix Zeroes.java

--- HashMap/73. Set Matrix Zeroes.java
+++ HashMap/73. Set Matrix Zeroes.java
@@ -1,4 +1,38 @@
 // Problem Link: https://leetcode.com/problems/set-matrix-zeroes/description/
+
+// hashSet approach
+class Solution {
+    public void setZeroes(int[][] matrix) {
+          int m = matrix.length;
+        int n = matrix[0].length;
+         // HashSets to store rows and columns that need to be zeroed
+        HashSet<Integer> zeroRows = new HashSet<>();
+        HashSet<Integer> zeroCols = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows.add(i);
+                    zeroCols.add(j);
+                }
+            }
+        }
+         // Step 2: Set rows to zero
+        for (int row : zeroRows) {
+            for (int j = 0; j < n; j++) {
+                matrix[row][j] = 0;
+            }
+        }
+
+           // Step 3: Set columns to zero
+        for (int col : zeroCols) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][col] = 0;
+            }
+        }
+    }
+}
+
+// OPTIMIZED APPROCH ----- RT=0ms
 class Solution {
     public void setZeroes(int[][] matrix) {
         boolean zeroinFirstCol = false;
@@ -24,3 +58,6 @@ class Solution {
         }
     }
 }
+
+
+
