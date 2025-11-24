827b0c0b03d60040773936c2155bf2ea5aed49a5
Divyanshi Sahu <sahudiv0309@gmail.com>
2025-11-24 16:42:51 +0530

    Update 349. Intersection of Two Arrays.java

--- HashMap/349. Intersection of Two Arrays.java
+++ HashMap/349. Intersection of Two Arrays.java
@@ -25,3 +25,21 @@ class Solution {
         return output;
     }
 }
+
+
+
+//OPTIMIZED APPROACH --------- RT=0ms
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        boolean[] seen = new boolean[1001];
+        for (int num : nums1)
+            seen[num] = true;
+        int index = 0;
+        for (int num : nums2) {
+            if (seen[num] == true)
+                nums1[index++] = num;
+            seen[num] = false;
+        }
+        return Arrays.copyOf(nums1, index);
+    }
+}
